314a1a7ba94ae4d036983ebc7a942f99bb8d9016
Rafid13iit <rafidssrr.6767@gmail.com>
2023-08-08 22:38:27 +0600

    files are updated

--- createTableBranch.sql
+++ createTableBranch.sql
@@ -1,6 +1,6 @@
 create table branch (
-    branch_name varchar(2),
-    branch_city varchar(2),
+    branch_name varchar2(30),
+    branch_city varchar2(30)),
     assets number,
     constraint branch_pk primary key (branch_name)
 );
--- createTableCustomer.sql
+++ createTableCustomer.sql
@@ -1,6 +1,6 @@
 create table customer (
-    customer_name varchar(2),
-    customer_street varchar(2),
-    customer_city varchar(2),
+    customer_name varchar2(30),
+    customer_street varchar2(30),
+    customer_city varchar2(30),
     constraint customer_pk primary key (customer_name)
 );
